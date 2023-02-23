2c41a0ec6027efa70369a3f9b6f0d9d4ee117d39
A. Emmanuel <37243305+asamoahe3@users.noreply.github.com>
2023-02-23 23:56:25 +0000

    initial commit

---
+++ colorit.go
@@ -0,0 +1,17 @@
+package colorit
+
+/*------Normal Colors-----*/
+var Black = "\033[0;30m"
+var Red = "\033[0;31m"
+var Green = "\033[0;32m"
+var Yellow = "\033[0;33m"
+var Blue = "\033[0;34m"
+var Purple = "\033[0;35m"
+var Cyan = "\033[0;36m"
+var White = "\033[0;37m"
+var Neutral = "\033[0m" // Color reset
+
+/*-----Bold Text Colors-----*/
+var Black_B = "\033[1;30m"
+var Red_B = "\033[1;31m"
+var Green_B = "\033[1;32m"
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asamoahe3/colorit
+
+go 1.20
