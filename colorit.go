883c5221ce81c7660874547d56b07b20c41bc3d6
A. Emmanuel <37243305+asamoahe3@users.noreply.github.com>
2023-02-24 13:45:05 +0000

    Added more color codes

--- colorit.go
+++ colorit.go
@@ -1,6 +1,6 @@
 package colorit
 
-/*------Normal Colors-----*/
+// Normal (0;)
 var Black = "\033[0;30m"
 var Red = "\033[0;31m"
 var Green = "\033[0;32m"
@@ -11,11 +11,82 @@ var Cyan = "\033[0;36m"
 var White = "\033[0;37m"
 var Neutral = "\033[0m" // Color reset
 
-/*-----Bold Text Colors-----*/
-var Black_B = "\033[1;30m"
-var Red_B = "\033[1;31m"
-var Green_B = "\033[1;32m"
+// Bold (1;)
+var BlackBold = "\033[1;30m"
+var RedBold = "\033[1;31m"
+var GreenBold = "\033[1;32m"
+var YellowBold = "\033[1;33m"
+var BlueBold = "\033[1;34m"
+var PurpleBold = "\033[1;35m"
+var CyanBold = "\033[1;36m"
+var WhiteBold = "\033[1;37m"
 
-var Colors = map[string]string{
-	"Red_U": "\033[4;31m",
-}
+// Italic (3;)
+var BlackItalic = "\033[3;30m"
+var RedItalic = "\033[3;31m"
+var GreenItalic = "\033[3;32m"
+var YellowItalic = "\033[3;33m"
+var BlueItalic = "\033[3;34m"
+var PurpleItalic = "\033[3;35m"
+var CyanItalic = "\033[3;36m"
+var WhiteItalic = "\033[3;37m"
+
+// Underline (4;)
+var BlackUnderline = "\033[4;30m"
+var RedUnderline = "\033[4;31m"
+var GreenUnderline = "\033[4;32m"
+var YellowUnderline = "\033[4;33m"
+var BlueUnderline = "\033[4;34m"
+var PurpleUnderline = "\033[4;35m"
+var CyanUnderline = "\033[4;36m"
+var WhiteUnderline = "\033[4;37m"
+
+// Blinking (5;)
+var BlackBlink = "\033[5;30m"
+var RedBlink = "\033[5;31m"
+var GreenBlink = "\033[5;32m"
+var YellowBlink = "\033[5;33m"
+var BlueBlink = "\033[5;34m"
+var PurpleBlink = "\033[5;35m"
+var CyanBlink = "\033[5;36m"
+var WhiteBlink = "\033[5;37m"
+
+// # Background (4*m)
+var BlackBG = "\033[40m"
+var RedBG = "\033[41m"
+var GreenBG = "\033[42m"
+var YellowBG = "\033[43m"
+var BlueBG = "\033[44m"
+var PurpleBG = "\033[45m"
+var CyanBG = "\033[46m"
+var WhiteBG = "\033[47m"
+
+// # High Intensty
+var BlackHI = "\033[0;90m"
+var RedHI = "\033[0;91m"
+var GreenHI = "\033[0;92m"
+var YellowHI = "\033[0;93m"
+var BlueHI = "\033[0;94m"
+var PurpleHI = "\033[0;95m"
+var CyanHI = "\033[0;96m"
+var WhiteHI = "\033[0;97m"
+
+// # High Intensty Bold
+var BlackHIB = "\033[1;90m"
+var RedHIB = "\033[1;91m"
+var GreenHIB = "\033[1;92m"
+var YellowHIB = "\033[1;93m"
+var BlueHIB = "\033[1;94m"
+var PurpleHIB = "\033[1;95m"
+var CyanHIB = "\033[1;96m"
+var WhiteHIB = "\033[1;97m"
+
+// # High Intensty Background
+var BlackHIBG = "\033[0;100m"
+var RedHIBG = "\033[0;101m"
+var GreenHIBG = "\033[0;102m"
+var YellowHIBG = "\033[0;103m"
+var BlueHIBG = "\033[0;104m"
+var PurpleHIBG = "\033[10;95m"
+var CyanHIBG = "\033[0;106m"
+var WhiteHIBG = "\033[0;107m"
